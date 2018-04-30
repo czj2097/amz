38cdf2ff747cfb1e73c1c0e04f008b15db66c159
czj <czj2097@gmail.com>
2018-04-30 16:00:41 +0800

    test well, but still need some improvement

--- get_asin.py
+++ get_asin.py
@@ -1,78 +1,91 @@
-# -*- coding: utf-8 -*-  
 import time
 import urllib2
 import re
 import xlwt
 from bs4 import BeautifulSoup
+
 from fake_useragent import UserAgent
 ua = UserAgent()
 headers = {'User-Agent' : ua.random}
 #ua = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0'
 #headers = {'User-Agent' : ua}
-addr = 'https://www.amazon.com/gp/new-releases/hi/511228/'
-newResult = []
-k=0
 
-while k<2:
-	req = urllib2.Request(addr,headers=headers)
+def get_ASIN(addr):
+	newResult = []
+	k=0
+	while True:
+		req = urllib2.Request(addr,headers=headers)
 
-	#Get page of 50 items, ignore page of 20 items
-	response = urllib2.urlopen(req)
-	raw_html = response.read()
-	bsObj = BeautifulSoup(raw_html,'html.parser')
-	req_num = 1
-	while bsObj.find("div",id="zg-center-div")==None and req_num<20:
-		time.sleep(10)
+		#Get page of 50 items, ignore page of 20 items
 		response = urllib2.urlopen(req)
 		raw_html = response.read()
 		bsObj = BeautifulSoup(raw_html,'html.parser')
-		req_num = req_num+1
-	if req_num == 20:
-		print("Error: url request 20 times, still get the wrong page, please check")
-		targetObj = bsObj.find("div",id="zg_centerListWrapper")
-	else:
-		print("Request times: "+str(req_num))
-		targetObj = bsObj.find("div",id="zg-center-div")
+		req_num = 1
+		while bsObj.find("div",id="zg-center-div")==None and req_num<20:
+			time.sleep(10)
+			response = urllib2.urlopen(req)
+			raw_html = response.read()
+			bsObj = BeautifulSoup(raw_html,'html.parser')
+			req_num = req_num+1
+		if req_num == 20:
+			print("Error: url request 20 times, still get the wrong page, please check")
+			targetObj = bsObj.find("div",id="zg_centerListWrapper")
+		else:
+			print("Request times: "+str(req_num))
+			targetObj = bsObj.find("div",id="zg-center-div")
 
-	#Get ASIN of current page
-	pattern_9 = re.compile(r'/B0......./',re.S)
-	pattern_10 = re.compile(r'/B0......../',re.S)
-	pattern_11 = re.compile(r'/B0........./',re.S)
-	result_9 = pattern_9.findall(str(targetObj))
-	result_10 = pattern_10.findall(str(targetObj))
-	result_11 = pattern_11.findall(str(targetObj))
-	for ele1 in result_9:
-		if ele1 not in newResult:
-			newResult.append(ele1)
-	for ele2 in result_10:
-		if ele2 not in newResult:
-			newResult.append(ele2)
-	for ele3 in result_11:
-		if ele3 not in newResult:
-			newResult.append(ele3)
+		#Get ASIN of current page
+		pattern_9 = re.compile(r'/B0......./',re.S)
+		pattern_10 = re.compile(r'/B0......../',re.S)
+		pattern_11 = re.compile(r'/B0........./',re.S)
+		result_9 = pattern_9.findall(str(targetObj))
+		result_10 = pattern_10.findall(str(targetObj))
+		result_11 = pattern_11.findall(str(targetObj))
+		for ele1 in result_9:
+			if ele1 not in newResult:
+				newResult.append(ele1)
+		for ele2 in result_10:
+			if ele2 not in newResult:
+				newResult.append(ele2)
+		for ele3 in result_11:
+			if ele3 not in newResult:
+				newResult.append(ele3)
 
-	#Get addr of next page
-	next_page = addr
-	all_a = targetObj.find_all("a")
-	for i in all_a:
-        	if i.find(text = "Next page") != None:
-                	target_a = i
-                	next_page = str(target_a.get('href'))
-                	print("next_page addr="+next_page)
-	if next_page == addr:
-		print("Next page not exist! Get "+str(k+1)+" pages in all.")
-		break;
-	else:
-		addr=next_page
-		k=k+1
+		#Get addr of next page
+		next_page = addr
+		all_a = targetObj.find_all("a")
+		for i in all_a:
+	        	if i.find(text = "Next page") != None:
+    		            	target_a = i
+                		next_page = str(target_a.get('href'))
+                		print("next_page addr="+next_page)
+		if next_page == addr:
+			print("Next page not exist! Get "+str(k+1)+" pages in all.")
+			break;
+		else:
+			addr=next_page
+			k=k+1
+	
+	return newResult
 
-print(newResult)
-print("ASIN number: "+str(len(newResult)))
+
+page = 'https://www.amazon.com/gp/new-releases/hi/511228/'
+ASINs = get_ASIN(page)
 
 wbk = xlwt.Workbook()
-sheet = wbk.add_sheet('sheet 1')
-line = 0
-for asin in newResult:
-	sheet.write(line,0,asin)
-	line = line + 1
-wbk.save('Product Details.xls')
+sheet_1 = wbk.add_sheet('AllASIN')
+for i in range(0,len(ASINs)):
+	sheet_1.write(i,0,ASINs[i])
+sheet_1.write(0,1,0)
+sheet_1.write(1,1,len(ASINs))
+sheet_1.write(2,1,0)
+sheet_1.write(3,1,0)
+
+
+Titles = ["ASIN","Review","Stars","Rank1","RankDetail1","Rank2","RankDetail2","Rank3","RankDetail3","Date"]
+sheet_2 = wbk.add_sheet('ProductDetails')
+for j in range(0,len(Titles)):
+	sheet_2.write(0,j,Titles[j])
+
+sheet_3 = wbk.add_sheet('ErrorASIN')
+wbk.save('ProductDetails.xls')
--- get_detail.py
+++ get_detail.py
@@ -1,57 +1,140 @@
 import urllib2
 import re
 from bs4 import BeautifulSoup
+
+import time
+import datetime
+
+import xlwt
+import xlrd
+from xlutils.copy import copy
+
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf-8')
+
 from fake_useragent import UserAgent
 ua = UserAgent()
 headers = {'User-Agent' : ua.random}
 
-#addr = 'https://www.amazon.com/dp/B07B6F5PJ7/'
-#req = urllib2.Request(addr,headers=headers)
-#response = urllib2.urlopen(req)
-#raw_html = response.read()
+def get_detail_of_oneASIN(oneASIN):
+	print("\nASIN:"+oneASIN)
+	details = []
+	errASIN = []
+	review = 0
+	star = 0.0
+	rank = [0,0,0]
+        category = ['0','0','0']
+	date = '0000-00-00'
 
-with open('../amazon_origin.html','r') as f:
-        raw_html = f.read()
+	addr = "https://www.amazon.com/dp"+oneASIN
+	req = urllib2.Request(addr,headers=headers)
+        response = urllib2.urlopen(req)
+        raw_html = response.read()
+	bsObj = BeautifulSoup(raw_html,'html.parser')
+	table = bsObj.find("table",id="productDetails_detailBullets_sections1")
+	if table == None:
+		errASIN.append(oneASIN)
+		print("Error: table not found, return errASIN")
+		return errASIN
+	else:
+		all_tr = table.find_all("tr")
+	for tr in all_tr:
+		if tr.find("th") == None or tr.find("td") ==None:
+			errASIN.append(oneASIN)
+        	        print("Error: th or td not found, return errASIN")
+	                return errASIN
 
-bsObj = BeautifulSoup(raw_html,'html.parser')
-all_tr = bsObj.find("table",id="productDetails_detailBullets_sections1").find_all("tr")
-for tr in all_tr:
-	item = tr.find("th").string
-	value = tr.find("td")
+		item = tr.find("th").string
+		value = tr.find("td")
 
-	if re.search(r'Customer Reviews',item):
-		review = '0'
-		star = '0'
-		all_a = value.find_all("a")
-		for a in all_a:
-			rw_pattern1 = re.compile(r'([0-9]*) customer reviews',re.I)
-			rw_pattern2 = re.compile(r'([0-9]*\.[0-9]*) out of 5 stars',re.I)
-			rw_target1 = rw_pattern1.search(str(a))
-			rw_target2 = rw_pattern2.search(str(a))
-			if rw_target1 != None:
-				review = rw_target1.group(1)
-			if rw_target2 != None:
-				star = rw_target2.group(1)
-		print(review)
-		print(star)
-	elif re.search(r'Best Sellers Rank',item):
-		rank = ['0','0','0']
-		category = ['0','0','0']
-		rk_k = 0
-		all_span = value.span.find_all("span")
-		for span in all_span:
-			rk_pattern1 = re.compile(r'#([0-9]*) in',re.I)
-			rank[rk_k] = rk_pattern1.search(str(span)).group(1)
-			category[rk_k] = str(span.get_text())
-			rk_k = rk_k+1
-		print(rank)
-		print(category)
+		if re.search(r'Customer Reviews',item):
+			all_a = value.find_all("a")
+			for a in all_a:
+				rw_pattern1 = re.compile(r'([0-9,]*) customer reviews',re.I)
+				rw_pattern2 = re.compile(r'([0-9\.]*) out of 5 stars',re.I)
+				rw_target1 = rw_pattern1.search(str(a))
+				rw_target2 = rw_pattern2.search(str(a))
+				if rw_target1 != None:
+					review = int(rw_target1.group(1).replace(',',''))
+				if rw_target2 != None:
+					star = float(rw_target2.group(1))
+			print("Get review successfully")
+			#print(review)
+			#print(star)
 
-	elif re.search(r'Date First Available',item):
-		date = ['0','0','0']
-		dt_pattern = re.compile(r'([a-z]*) ([0-9]*), ([0-9]*)',re.I)
-		dt_target = dt_pattern.search(tr.find("td").string)
-		date[0] = str(dt_target.group(1))
-		date[1] = str(dt_target.group(2))
-		date[2] = str(dt_target.group(3))
-		print(date)
+		elif re.search(r'Best Sellers Rank',item):
+			rk_k = 0
+			all_span = value.span.find_all("span")
+			for span in all_span:
+				rk_pattern = re.compile(r'#([0-9,]*) in',re.I)
+				rk_target = rk_pattern.search(str(span))
+				if rk_target != None:
+					rank[rk_k] = int(rk_target.group(1).replace(',',''))
+					category[rk_k] = str(span.get_text())
+				rk_k = rk_k+1
+				if rk_k == 3:
+					break;
+			print("Get rank successfully")
+			#print(rank)
+			#print(category)
+
+		elif re.search(r'Date First Available',item):
+			dt_pattern = re.compile(r'([a-z]*) ([0-9]*), ([0-9]*)',re.I)
+			dt_target = dt_pattern.search(tr.find("td").string)
+			if dt_target != None:
+				dt_format = datetime.datetime.strptime(str(dt_target.group()),'%B %d, %Y')
+				date = dt_format.strftime('%Y-%m-%d')
+			print("Get date successfully")
+			#print(date)
+
+	details.append(oneASIN)
+	details.append(review)
+	details.append(star)
+	details.append(rank[0])
+	details.append(category[0])
+	details.append(rank[1])
+	details.append(category[1])
+	details.append(rank[2])
+	details.append(category[2])
+	details.append(date)
+
+	return details
+
+
+while True:
+	wbk_rd = xlrd.open_workbook('ProductDetails.xls')
+	asin_sheet_rd = wbk_rd.sheet_by_name('AllASIN')
+	index_now = int(asin_sheet_rd.cell(0,1).value)
+	index_end = int(asin_sheet_rd.cell(1,1).value)
+	row_nml = int(asin_sheet_rd.cell(2,1).value)
+	row_err = int(asin_sheet_rd.cell(3,1).value)
+	asin = asin_sheet_rd.cell(index_now,0).value
+
+	wbk_wt = copy(wbk_rd)
+
+	asin_detail = get_detail_of_oneASIN(asin)
+	if len(asin_detail)==1:
+		sheet_err = wbk_wt.get_sheet('ErrorASIN')
+		sheet_err.write(row_err,0,asin_detail[0])
+		row_err = row_err+1
+	else:
+		for i in range(0,len(asin_detail)):
+			sheet_nml = wbk_wt.get_sheet('ProductDetails')
+			if isinstance(asin_detail[i],str):
+				detail_tmp = asin_detail[i].decode('utf-8')
+			else:
+				detail_tmp = asin_detail[i]
+			sheet_nml.write(row_nml,i,detail_tmp)
+		row_nml = row_nml + 1
+	asin_sheet_wt = wbk_wt.get_sheet('AllASIN')
+	index_now = index_now + 1
+	asin_sheet_wt.write(0,1,index_now)
+	asin_sheet_wt.write(2,1,row_nml)
+	asin_sheet_wt.write(3,1,row_err)
+	
+	wbk_wt.save('ProductDetails.xls')
+
+	if index_now == index_end:
+		break
+
